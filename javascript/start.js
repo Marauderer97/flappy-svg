63aa62ccd43309dbe1c19445954599cf6a6b36c1
Soumya Vadlamannati <vadlamannati1997@gmail.com>
2017-01-13 23:55:55 +0530

    Revert "Fixes #253 Adds Keyboard Control and updates the how-to-play svg"

--- javascript/start.js
+++ javascript/start.js
@@ -1,15 +1,14 @@
 // Execute everything that should be started.
 // This script must be the last script. Otherwise not all functions may be loaded.
 
-var characters;
-var backgrounds;
+
 
 window.onload = function() {
     scaleToFullscreen();
     showStartScreen();
 
-    characters = ["bird", "bat", "alien","flappydino","helicopter", "rocket","Santa", "Super_rocket", "Flappyfish",  "unicorn", "botty", "black_cat","ball","octodex", "grandma","dog"];
-    backgrounds=["background","Gotham","space","Snow", "Sea", "Island", "binarybg","Evil"];
+    var characters = ["bird", "bat", "alien","flappydino","helicopter", "rocket","Santa", "Super_rocket", "Flappyfish",  "unicorn", "botty", "black_cat","ball","octodex", "grandma","dog"];
+    var backgrounds=["background","Gotham","space","Snow", "Sea", "Island", "binarybg","Evil"];
 
     rand=Math.floor((Math.random() * (backgrounds.length)));
     backgroundChange(backgrounds[rand])
@@ -17,32 +16,4 @@ window.onload = function() {
     rand = Math.floor((Math.random() * (characters.length)));
     characterChange(characters[rand])
 
-}
-i=0;
-j=0;
-window.onkeydown = function(event){
-    switch(event.keyCode){
-        case 32:
-            var layer=characters[i];
-            console.log(characters[i]);
-            characterChange(layer);
-            i=(i+1)%(characters.length);
-            break;
-        case 66:
-            var layer=backgrounds[j];
-            console.log(backgrounds[j]);
-            backgroundChange(layer);
-            j=(j+1)%(backgrounds.length);
-            break;
-        case 13:
-            startGame();
-            break;
-        case 82:
-            restartGame();
-            break;
-        default:
-            console.log("enterred");
-            break;
-    }
-    return false;
 }
